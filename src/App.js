99a914630daddbf314e1b60f07a22e107fad63e6
Aratrika Ray <aratrika.ray1999@gmail.com>
2020-06-08 03:07:01 +0530

    new upload

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import WeatherCard from './weatherCard';
+import './App.css';
+
+function convertToMinutes(time) {
+    return Number(time.split(':')[0]) * 60;
+}
+
+const WeatherInfo = (props) => {
+    const data0 = props.response.list[0];
+    const data1 = props.response.list[1];
+    const data2 = props.response.list[2];
+    const hours = (new Date().getHours() * 60) + (new Date().getMinutes());
+
+    if (hours > convertToMinutes(data0.dt_txt.split(" ")[1])) {
+
+        if (hours <= convertToMinutes(data1.dt_txt.split(" ")[1])) {
+            return (
+                <div>
+                    <p>{new Date().toLocaleTimeString().replace(/:\d+ /, ' ')}, {new Date().toLocaleDateString('es-CL')}</p>
+                    <p>Temperature: {Math.floor((data0.main.temp + data1.main.temp) / 2)} °C</p>
+                    <p>Min: {Math.floor((data0.main.temp_min + data1.main.temp_min) / 2)} °C</p>
+                    <p>Max: {Math.floor((data0.main.temp_max + data1.main.temp_max) / 2)} °C</p>
+                    <p>Rain: {data0.weather[0].description}</p>
+                </div>
+            );
+        }
+
+        if (hours >= convertToMinutes(data1.dt_txt.split(" ")[1]) + 105) {
+            return (
+                <div>
+                    <p>{new Date().toLocaleTimeString().replace(/:\d+ /, ' ')}, {new Date().toLocaleDateString('es-CL')}</p>
+                    <p>Temperature: {Math.floor((data1.main.temp + data2.main.temp) / 2)} °C</p>
+                    <p>Min: {Math.floor((data1.main.temp_min + data2.main.temp_min) / 2)} °C</p>
+                    <p>Max: {Math.floor((data1.main.temp_max + data2.main.temp_max) / 2)} °C</p>
+                    <p>Rain: {data2.weather[0].description}</p>
+                </div>
+            );
+        }
+
+        if (hours >= convertToMinutes(data1.dt_txt.split(" ")[1])) {
+            return (
+                <div>
+                    <p>{new Date().toLocaleTimeString().replace(/:\d+ /, ' ')}, {new Date().toLocaleDateString('es-CL')}</p>
+                    <p>Temperature: {(data1.main.temp)} °C</p>
+                    <p>Min: {(data1.main.temp_min)} °C</p>
+                    <p>Max: {(data1.main.temp_max)} °C</p>
+                    <p>Rain: {data1.weather[0].description}</p>
+                </div>
+            );
+        }
+
+    }
+    else if(hours < convertToMinutes(data0.dt_txt.split(" ")[1]) && (convertToMinutes(data0.dt_txt.split(" ")[1]) === 1260)) {
+        return (
+            <div>
+                <p>{new Date().toLocaleTimeString().replace(/:\d+ /, ' ')}, {new Date().toLocaleDateString('es-CL')}</p>
+                <p>Temperature: {(data1.main.temp)} °C</p>
+                <p>Min: {(data1.main.temp_min)} °C</p>
+                <p>Max: {(data1.main.temp_max)} °C</p>
+                <p>Rain: {data1.weather[0].description}</p>
+            </div>
+        );
+    }
+    else return null;
+}
+
+
+class App extends React.Component {
+
+    state = {
+        res: []
+    }
+
+    async componentDidMount() {
+        const api = 'b1ed46552dc89999f5a3e717d5316c4f';
+        const res = await fetch('https://api.openweathermap.org/data/2.5/forecast?id=1277333&units=metric&appid=' + api);
+        const data = await res.json();
+        this.setState({ res: data });
+        console.log(data);
+    }
+
+    render() {
+        if (this.state.res.length === 0) return null;
+        const hours = new Date().getHours();
+        const name = (hours < new Date(this.state.res.city.sunrise * 1000).getHours()) ||
+            (hours > new Date(this.state.res.city.sunset * 1000).getHours()) ? 'night' : 'day';
+        
+            return (
+            <div className={name}>
+                <div className='topCard'>
+                    <WeatherInfo response={this.state.res} />
+                </div>
+                <div className='weathers'>
+                    <WeatherCard response={this.state.res} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/weatherCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './weatherCard.css';
+
+const WeatherDeatils = (props) => {
+    const data = props.data;
+    //<p>At time: {new Date().toLocaleTimeString().replace(/:\d+ /, ' ')}</p>
+    const tomorrow = props.tomorrow === true ? 
+                    <p>Tomorrow at: {new Date(props.time).toLocaleTimeString()}</p> :
+                    <p>At: {new Date(props.time).toLocaleTimeString()}</p>;
+    return (
+        <div>
+            {tomorrow}
+            <p>Temperature: {data.main.temp} °C</p>
+            <p>Min: {data.main.temp_min} °C</p>
+            <p>Max: {data.main.temp_max} °C</p>
+            <p>Rain: {data.weather[0].description}</p>
+        </div>
+    );
+}
+
+const WeatherCard = (props) => {
+    var arrayList = props.response.list.slice(1, 5);
+    console.log(arrayList);
+    var tmrw = true;
+    return arrayList.map((list) => {
+        if (new Date().getDate() === new Date(list.dt_txt).getDate()) tmrw = false;
+        return(
+            <div className='card'>
+                <WeatherDeatils data={list} time={list.dt_txt} tomorrow={tmrw}/>
+            </div>
+        );
+    });
+
+}
+export default WeatherCard;
