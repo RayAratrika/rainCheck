7392dec4a7b72f411dda8f4243b0735c0a580ea1
Aratrika Ray <aratrika.ray1999@gmail.com>
2020-06-08 22:04:04 +0530

    Updates

---
+++ precache-manifest.46ad1514145de29253510de4b3cec367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23a31064f9bd867731d44b2997c29d3",
+    "url": "/rainCheck/index.html"
+  },
+  {
+    "revision": "1ee56ffc1a535802f2c7",
+    "url": "/rainCheck/static/css/main.daf665ac.chunk.css"
+  },
+  {
+    "revision": "d8e98b1705b2d46db17d",
+    "url": "/rainCheck/static/js/2.d2c3cf04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rainCheck/static/js/2.d2c3cf04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee56ffc1a535802f2c7",
+    "url": "/rainCheck/static/js/main.af823441.chunk.js"
+  },
+  {
+    "revision": "42d5e5cc70f23d30a9bb",
+    "url": "/rainCheck/static/js/runtime-main.8ace6c53.js"
+  }
+]);
